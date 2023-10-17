3fed50a6f42c2eb32184a1e221c81c4a240b1e7f
garima2004yadav <115277218+garima2004yadav@users.noreply.github.com>
2023-10-17 22:32:53 +0530

    Add files via upload

---
+++ Secondmax.java
@@ -0,0 +1,14 @@
+public class Secondmax {
+    public static void main(String[] args) {
+     int arr[] = {5,8,1,3,2};   
+     int max = arr[0];
+     for(int i=1;i>arr.length;i--){
+      if (max > arr[i]){
+        max = arr[i];
+        }
+     }
+     System.out.println("Max Element is" + max);
+        
+    }
+    
+}
